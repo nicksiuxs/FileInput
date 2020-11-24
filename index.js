6caaf8e594b711b3223c8643cdcde236b5dcebaf
Nicolas <nicksiuxs@gmail.com>
2020-11-24 13:55:21 -0500

    Testing drag and drop

--- index.js
+++ index.js
@@ -1,167 +1,156 @@
 /* Create one Fake input file */
-var fakeInput = document.createElement( "input" );
+var fakeInput = document.createElement("input");
 fakeInput.type = "file";
 fakeInput.accept = "image/*";
 fakeInput.multiple = true;
+// var formData = new FormData();
+var formData = new Map();
 
+var dropRegion = document.getElementById("drop-region");
 
-var dropRegion = document.getElementById( "drop-region" )
-
-let imagePreviewRegion = document.getElementById("image-preview");
+var imagePreviewRegion = document.getElementById("image-preview");
 
 /* Detect Onclick into region */
-$( "#drop-region" ).click( function ( e ) {
-    e.preventDefault();
-    fakeInput.click()
-} );
+$("#drop-region").click(function (e) {
+  e.preventDefault();
+  fakeInput.click();
+});
 
 /* Detect onclick */
-fakeInput.addEventListener( "change", function () {
-    var files = fakeInput.files;
-    console.log( "fakeInput.addEventListener -> files", files )
+fakeInput.addEventListener("change", function () {
+  var files = fakeInput.files;
+  console.log("fakeInput.addEventListener -> files", files);
+  handleFiles(files);
+});
+
+const preventDefault = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+};
+
+dropRegion.addEventListener("dragenter", preventDefault, false);
+dropRegion.addEventListener("dragleave", preventDefault, false);
+dropRegion.addEventListener("dragover", preventDefault, false);
+dropRegion.addEventListener("drop", preventDefault, false);
+
+/**
+ * handle the drop event of a image
+ * @param {Event} e : event drop
+ * @return {Void}
+ */
+function handleDrop(e) {
+  var dt = e.dataTransfer,
+    files = dt.files;
+
+  if (files.length) {
     handleFiles(files);
-} );
-
-const preventDefault = ( e ) => {
-    e.preventDefault()
-    e.stopPropagation()
+  } else {
+    // check for img
+    var html = dt.getData("text/html"),
+      match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
+      url = match && match[1];
+    console.log("handleDrop -> url", url);
+    console.log("handleDrop -> match", match);
+    console.log("handleDrop -> html", html);
+    if (url) {
+      // uploadImageFromURL( url );
+      return;
+    }
+  }
 }
 
-dropRegion.addEventListener( 'dragenter', preventDefault, false )
-dropRegion.addEventListener( 'dragleave', preventDefault, false )
-dropRegion.addEventListener( 'dragover', preventDefault, false )
-dropRegion.addEventListener( 'drop', preventDefault, false )
+dropRegion.addEventListener("drop", handleDrop, false);
 
-
-function handleDrop ( e ) {
-    var dt = e.dataTransfer, files = dt.files;
-
-    if ( files.length ) {
-        handleFiles( files );
-    } else {
-        // check for img
-        var html = dt.getData( 'text/html' ),
-        match = html && /\bsrc="?([^"\s]+)"?\s*/.exec( html ),
-        url = match && match[1];
-        console.log("handleDrop -> url", url)
-        console.log("handleDrop -> match", match)
-        console.log("handleDrop -> html", html)
-        if ( url ) {
-            // uploadImageFromURL( url );
-            return;
-        }
-    }
-
-    function uploadImageFromURL ( url ) {
-        var img = new Image;
-        var c = document.createElement( "canvas" );
-        var ctx = c.getContext( "2d" );
-
-        img.onload = function () {
-            c.width = this.naturalWidth;     // update canvas size to match image
-            c.height = this.naturalHeight;
-            ctx.drawImage( this, 0, 0 );       // draw in image
-            c.toBlob( function ( blob ) {        // get content as PNG blob
-
-                // call our main function
-                handleFiles( [blob] );
-
-            }, "image/png" );
-        };
-        img.onerror = function () {
-            alert( "Error in uploading" );
-        }
-        img.crossOrigin = "";              // if from different origin
-        img.src = url;
-    }
-}
-
-dropRegion.addEventListener('drop', handleDrop, false);
-
-
-
+/**
+ * Shows the images in a area
+ * @param {File} files : image in a format
+ * @return {Void}
+ */
 function handleFiles(files) {
-	for (var i = 0, len = files.length; i < len; i++) {
-		if (validateImage(files[i]))
-			previewAnduploadImage(files[i]);
-	}
+  for (let i = 0, len = files.length; i < len; i++) {
+    if (validateImage(files[i])) previewAndUploadImage(files[i]);
+  }
 }
 
-/* Validate if is a image */
+/**
+ * Validates the format of a Image
+ * @param {File} image : image to validate
+ * @return {Boolean} : if the image isa validate
+ */
 function validateImage(image) {
-	// check the type
-	var validTypes = ['image/jpeg', 'image/png', 'image/gif'];
-	if (validTypes.indexOf( image.type ) === -1) {
-		alert("Invalid File Type");
-		return false;
-	}
+  // check the type
+  var validTypes = ["image/jpeg", "image/png", "image/gif"];
+  if (validTypes.indexOf(image.type) === -1) {
+    alert(
+      "El archivo no es un tipo válido, recuerde que los tipos válidos son JPG o PNG "
+    );
+    return false;
+  }
 
-	// check the size
-	var maxSizeInBytes = 10e6; // 10MB
-	if (image.size > maxSizeInBytes) {
-		alert("File too large");
-		return false;
-	}
+  // check the size
+  var maxSizeInBytes = 10e6; // 10MB
+  if (image.size > maxSizeInBytes) {
+    alert("File too large");
+    return false;
+  }
 
-	return true;
+  return true;
 }
 
-function previewAnduploadImage(image) {
+/**
+ * Preview images in an area and upload
+ * @param {File} image : the image to preview and upload
+ * @return {Void}
+ */
+function previewAndUploadImage(image) {
+  // container
+  var imgView = document.createElement("div");
+  imgView.className = "image-view";
+  imagePreviewRegion.appendChild(imgView);
 
-	// container
-	var imgView = document.createElement("div");
-	imgView.className = "image-view";
-	imagePreviewRegion.appendChild(imgView);
+  //image and text container
+  var imgText = document.createElement("div");
+  imgText.className = "image-text-container";
+  imgView.appendChild(imgText);
 
-	// previewing image
-	var img = document.createElement("img");
-	imgView.appendChild(img);
+  // previewing image
+  var img = document.createElement("img");
+  imgText.appendChild(img);
 
-	// progress overlay
-	var overlay = document.createElement("div");
-	overlay.className = "overlay";
-	imgView.appendChild(overlay);
+  // text preview image
+  var textImage = document.createElement("span");
+  var text = document.createTextNode(image.name);
+  textImage.appendChild(text);
+  imgText.appendChild(textImage);
 
+  //create the remove button
+  var removeButton = document.createElement("button");
+  removeButton.className = "xButton";
+  imgView.appendChild(removeButton);
+  removeButton.onclick = () => {
+    imagePreviewRegion.removeChild(imgView);
+    formData.delete(image.name);
+  };
 
-	// read the image...
-	var reader = new FileReader();
-	reader.onload = function(e) {
-		img.src = e.target.result;
-	}
-	reader.readAsDataURL(image);
+  //Add the svg to button
+  var icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-	// create FormData
-	var formData = new FormData();
-	formData.append('image', image);
+  // read the image...
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    img.src = e.target.result;
+  };
+  reader.readAsDataURL(image);
 
-	// upload the image
-	var uploadLocation = 'https://api.imgbb.com/1/upload';
-	formData.append('key', 'bb63bee9d9846c8d5b7947bcdb4b3573');
+  // create FormData
+  //   var formData = new FormData();
+  formData.set(image.name, image);
 
-	var ajax = new XMLHttpRequest();
-	ajax.open("POST", uploadLocation, true);
+  console.log(formData);
 
-	ajax.onreadystatechange = function(e) {
-		if (ajax.readyState === 4) {
-			if (ajax.status === 200) {
-				// done!
-			} else {
-				// error!
-			}
-		}
-	}
-
-	ajax.upload.onprogress = function(e) {
-
-		// change progress
-		// (reduce the width of overlay)
-
-		var perc = (e.loaded / e.total * 100) || 100,
-			width = 100 - perc;
-
-		overlay.style.width = width;
-	}
-
-	// ajax.send(formData);
-
+  document.getElementById("images").name = formData.get(image.name);
+  console.log(document.getElementById("images").name);
+  //Se da el valor al input
+  //   document.getElementById("images").name = formData.getAll("image");
+  //   console.log(formData);
 }
